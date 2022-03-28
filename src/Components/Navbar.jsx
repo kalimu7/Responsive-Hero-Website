42010920faf497c51d03aa70c8914c64ce579140
kalimu7 <mahjourkarim@gmail.com>
2022-03-28 14:49:43 +0000

    nav bar is done

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,118 +1,35 @@
-import React from 'react'
-import styled from 'styled-components';
+import React ,{useRef} from 'react'
+
 import { FaTimes } from 'react-icons/fa';
 import { CgMenuRight } from 'react-icons/cg';
 import { IconContext } from 'react-icons';
-import './nav.css';
-const Container = styled.div`
-    z-index: 5;
-    width: 100%;
-    margin-top:30px ;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    color:#fff;
-    position: absolute;
+import './stylesNav.css';
 
-    @media (max-width: 600px){
-        /* margin-top: 0;
-        padding-top: 30px;
-        background-color:#78808f ;
-        height: calc(100vh);
-        width: 100vw;
-        align-items: flex-start; */
-        display: block;
-        
-    }
 
-`
-const Logo = styled.img`
-    width: 60px;
-    height: 60px;
-    margin: 0 20px;
-`
-const Right = styled.div`
-    display: flex;
-    align-items: center;
-    color: #000;
-    flex: 1;
-    color: #fff;
-    @media (max-width: 600px){
-        width: 100%;
-        padding-bottom: 40px;
-        
-    }
-`
-
-const Left = styled.div`
-    display: flex;
-    flex: 1;
-    justify-content: space-around;
-    @media (max-width: 600px){
-        display: block;
-        height: calc(100vh - 130px);
-        width: 100%;
-        text-align: center;
-        background-color: #294b51;
-        position: fixed;
-        left: -100%;
-        transition: all 0.8s;
-
-    }
-`
-
-const NavLink = styled.h3`
-    
-    a{
-        text-decoration: none;
-        color: #fff;
-        cursor: pointer;
-    }
-    @media(max-width:600px){
-        padding:80px 0px ;
-    }
-    
-`
-
-const Input = styled.input`
-    display: none;
-     
-`
-const Label = styled.label`
-    display: none;
-    @media (max-width: 600px){
-        font-size: 30px;
-        display: block;
-        width: 100%;
-        text-align: end;
-        padding-right: 30px;
-        cursor: pointer;
-        
-    }
-    
-`
 
 function Navbar() {
+    const Nav_ref = useRef();
+    const ShowHide = ()=>{
+        Nav_ref.current.classList.toggle("resp_class");
+    }
   return (
-    <div>
-        <Container>
-            <Right>
-                <Logo src='./assets/logo.png' alt='logo' />
-                <h3>Delta</h3>
-            </Right>
-            <Left >
-                <NavLink><a href="#">About</a></NavLink>
-                <NavLink><a href="#">Pricing</a></NavLink>
-                <NavLink><a href="#">Contact</a></NavLink>
-                <button>
+
+   
+
+    <div className='Container'>
+        <img src="./assets/logo.png" className='Logo' />
+        <div ref={Nav_ref} className="links">
+            <a href="#">About</a>
+            <a href="#">Pricing</a>
+            <a href="#">Contact</a>
+            <button onClick={ShowHide} className='Nav_btn btn_Fatimes'>
                 <FaTimes/>
-                </button>
-            </Left>
-            <button>
-            <CgMenuRight />
             </button>
-            
-        </Container>
+        </div>
+        <button onClick={ShowHide} className='Nav_btn btn_Menu'>
+        <CgMenuRight />
+        </button>
+        
     </div>
   )
 }
